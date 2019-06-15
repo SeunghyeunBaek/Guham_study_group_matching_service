2df984f0ba1b844d81e1eee2f61294516ba64317
Seunghyeun Baek <wonca14@ajou.ac.kr>
2019-06-15 12:33:16 +0900

    add user on class Post | 190615

--- guham/posts/models.py
+++ guham/posts/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.conf import settings
+
+user = settings.AUTH_USER_MODEL  # user
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     summary = models.TextField()
     content = models.TextField()
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+
