16cc4570819905836c65d921b664a632dac7e50e
Seunghyeun Baek <wonca14@ajou.ac.kr>
2019-06-15 12:27:07 +0900

    add logout | 190615

--- guham/accounts/views.py
+++ guham/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 def signup(request):
@@ -33,3 +34,9 @@ def login(request):
         'form': form,
     }
     return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
+
