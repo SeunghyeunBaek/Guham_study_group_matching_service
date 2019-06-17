9e4abe0d7275c4395146983b2ad2aae2573023a8
Kuseul <doublet7411@naver.com>
2019-06-17 17:44:55 +0900

    models.py 수정

--- guham/posts/models.py
+++ guham/posts/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 # TODO user = settings.AUTH_USER_MODEL  # user
 
-
+# TODO 수정했습니다.
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    summary = models.TextField()
+    study_category = models.CharField(max_length=50, default='ALL')
     content = models.TextField()
+    summary = models.TextField()
     # TODO user = models.ForeignKey(user, on_delete=models.CASCADE)
