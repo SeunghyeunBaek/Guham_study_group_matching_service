883a2dc8020d0050eebff15c1496e48a00e2cbc4
Seunghyeun Baek <wonca14@ajou.ac.kr>
2019-06-18 19:43:13 +0900

    posts/models.py: add user, posts/templates/base.html: add user_page link, accounts: add user page | baek | 190618

--- guham/accounts/models.py
+++ guham/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
--- guham/match/forms.py
+++ guham/match/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import MatchPost
 
+
 class MatchPostForm(ModelForm):
     class Meta:
         model = MatchPost
