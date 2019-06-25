27bfec447d801998f31c2abae76b11e5c5cc5f12
Kuseul <doublet7411@naver.com>
2019-06-25 13:46:20 +0900

    test

--- guham/match/admin.py
+++ guham/match/admin.py
@@ -5,7 +5,6 @@ from import_export import resources
 
 
 class MatchPostResource(resources.ModelResource):
-
     class Meta:
         model = MatchPost
 
@@ -14,6 +13,15 @@ class MatchPostAdmin(ImportExportModelAdmin):
     resource_class = MatchPostResource
 
 
+class HashTagResource(resources.ModelResource):
+    class Meta:
+        model = HashTag
+
+
+class HashTagAdmin(ImportExportModelAdmin):
+    resource_class = HashTagResource
+
+
 # Register your models here.
 admin.site.register(MatchPost, MatchPostAdmin)
-admin.site.register(HashTag)
+admin.site.register(HashTag, HashTagAdmin)
